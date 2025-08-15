8537568f3574147ee0644094584d35a61da3dcf0
jhosefmmmmmm <166185410+jhosefmmmmmm@users.noreply.github.com>
2025-08-15 10:35:14 -0500

    Add files via upload

---
+++ src/app/tab5/tab5.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-tab5',
+  templateUrl: 'tab5.page.html',
+  styleUrls: ['tab5.page.scss'],
+  standalone: false,
+})
+export class Tab5Page {
+  constructor() {}
+ 
+}
